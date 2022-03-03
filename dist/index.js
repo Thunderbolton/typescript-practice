c42c76099e90a3919047186a74461c7739f7b492
Adam Bolton <adam_bolton90@hotmail.com>
2022-03-03 23:01:16 +0900

    updated ts file with basics of typescript

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,24 @@
 "use strict";
-let id = 1;
+// Basic types
+let id; // initalise
+let age = 10;
+let person = 'John';
+let isActive = true;
+let i = 'test'; // can be set to anything after initial declaration
+i = true;
+// arrays
+let ages = [1, 2, 3, 4, 5]; // the type of values in this array will only be numbers
+let names = ['john', 'james', 'tom'];
+let arr = ['john', 1, true];
+// tuple -- specify exact types in an array in order
+let profile = ['john', 1, false, 2];
+// an array of tuples
+let people;
+people = [
+    ['james', 1, true],
+    ['john', 2, false],
+    ['bill', 3, true],
+];
+// Union - variables that can hold more than one type
+let code;
+code = true;
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,27 @@
-let id: number = 1
+// Basic types
+let id: number // initalise
+let age: number = 10
+let person: string = 'John'
+let isActive: boolean = true
 
+let i: any = 'test' // can be set to anything after initial declaration
+i = true
+
+// arrays
+let ages: number[] = [1, 2, 3, 4, 5] // the type of values in this array will only be numbers
+let names: string[] = ['john', 'james', 'tom']
+let arr: any[] = ['john', 1, true]
+
+// tuple -- specify exact types in an array in order
+let profile: [string, number, boolean, number] = ['john', 1, false, 2]
+// an array of tuples
+let people: [string, number, boolean][]
+people = [
+    ['james', 1, true],
+    ['john', 2, false],
+    ['bill', 3, true],
+]
+
+// Union - variables that can hold more than one type
+let code: string | boolean
+code = true
